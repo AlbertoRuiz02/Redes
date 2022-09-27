a3e14078e8e6dec0b48140401b5d914a150cce23
AlbertoRuiz02 <90718032+AlbertoRuiz02@users.noreply.github.com>
2022-09-27 18:19:50 +0200

    P1 de Redes
    Falta terminar

---
+++ P1/servidorUDP.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <locale.h>
+
+
+/*
+ * El servidor ofrece el servicio de incrementar un n\ufffdmero recibido de un cliente
+ */
+ 
+int main ()
+{
+	/*---------------------------------------------------- 
+		Descriptor del socket y buffer de datos                
+	-----------------------------------------------------*/
+	int Socket_Servidor;
+	struct sockaddr_in Servidor;
+	char cadena[256];
+	
+
+	/* -----------------------------------------------------
+   		Informaci\ufffdn del Cliente
+   	-----------------------------------------------------*/
+   	struct sockaddr_in Cliente;
+	socklen_t Longitud_Cliente;
+	
+	
+	/* --------------------------------------------------
+		Se abre el socket Servidor
+	---------------------------------------------------*/
+	Socket_Servidor = socket (AF_INET, SOCK_DGRAM, 0);
+	if (Socket_Servidor == -1)
+	{
+		printf ("No se puede abrir socket servidor\n");
+		exit (-1);	
+	}
+
+	/* ------------------------------------------------------------------
+		Se rellenan los campos de la estructura servidor, necesaria
+		para la llamada a la funcion bind().
+	-------------------------------------------------------------------*/
+	Servidor.sin_family = AF_INET;
+	Servidor.sin_port = htons(2000);
+	Servidor.sin_addr.s_addr = htonl(INADDR_ANY); 
+
+	if (bind (Socket_Servidor, (struct sockaddr *)&Servidor, sizeof (Servidor)) == -1)
+	{
+		close (Socket_Servidor);
+		exit (-1);
+	}
+	
+
+  	/*---------------------------------------------------------------------
+		Del cliente s\ufffdlo necesitamos el tama\ufffdo de su estructura, el 
+		resto de informaci\ufffdn (familia, puerto, ip), la obtendremos 
+		nos la proporcionar\ufffd el propio m\ufffdtodo recvfrom, cuando
+		recibamos la llamada de un cliente.
+   	----------------------------------------------------------------------*/
+	Longitud_Cliente = sizeof (Cliente);
+
+   	
+	/*-----------------------------------------------------------------------
+		El servidor espera continuamente los mensajes de los clientes
+	------------------------------------------------------------------------ */
+	while (1)
+	{
+    	/* -----------------------------------------------------------------
+		Esperamos la llamada de alg\ufffdn cliente
+	-------------------------------------------------------------------*/
+      	int recibido = recvfrom (Socket_Servidor, &cadena, sizeof(cadena), 0,
+			(struct sockaddr *) &Cliente, &Longitud_Cliente);
+
+
+
+	/* -----------------------------------------------------------------
+		Comprobamos si hemos recibido alguna informaci\ufffdn 
+	-------------------------------------------------------------------*/
+	if (recibido > 0)
+	{
+		/*-----------------------------------------------------------------
+			Leemos la palabra recibida e imprimimos lo que quiera 
+		------------------------------------------------------------------*/
+
+	time_t tiempo;
+	struct tm * stTm;
+
+	tiempo = time(NULL);
+
+	setlocale(LC_ALL,"");
+	
+	stTm = localtime(&tiempo);
+
+
+			if(strcmp(cadena, "DAY") == 0){
+				strftime(cadena,256,"%A, %d de %B de %Y", stTm);
+				printf("Cadena: %s\n", cadena);
+			}
+
+			else if(strcmp(cadena, "TIME") == 0){
+				strftime(cadena,256,"%H:%M:%S", stTm);
+				printf("Cadena: %s\n", cadena);
+			}
+
+			else if(strcmp(cadena, "DAYTIME") == 0){
+				strftime(cadena,256,"%A, %d de %B de %Y;%H:%M:%S", stTm);
+				printf("Cadena: %s\n", cadena);
+			}
+			else if(strcmp(cadena, "DAY") == 0){
+				printf("Error, la palabra recibida debe ser: DAY | TIME | DAYTIME\n");	
+			}
+      
+      	
+      	/* ------------------------------------------------------------------
+		Devolvemos el n\ufffdmero incrementado al cliente
+		--------------------------------------------------------------------*/
+		int enviado = sendto (Socket_Servidor, &cadena, sizeof(cadena), 0,
+			(struct sockaddr *) &Cliente, Longitud_Cliente);
+
+		}
+
+    	}
+	 
+	 close(Socket_Servidor);
+
+	return 0;
+}
